6afd09538c3bdca8459c6f6adc950d0cb2922df1
Gamevalentine <anhtsizoo00@gmail.com>
2026-09-05 15:40:23 +0700

    GĐ3: connect game over actions to checkpoints

--- game_over.gd
+++ game_over.gd
@@ -1,6 +1,19 @@
 extends CanvasLayer
 
+@onready var continue_button: Button = %Continue
 
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	continue_button.disabled = not CheckpointManager.has_checkpoint()
+
+func _on_continue_pressed() -> void:
+	if not CheckpointManager.continue_from_checkpoint():
+		continue_button.disabled = true
+
+func _on_restart_pressed() -> void:
+	CheckpointManager.start_new_game()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
