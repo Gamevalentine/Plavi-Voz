96f06731d0b51a704f4453491f690469065db337
Gamevalentine <anhtsizoo00@gmail.com>
2026-09-05 15:46:33 +0700

    GĐ4: make car inspection require E interaction

--- car.gd
+++ car.gd
@@ -7,11 +7,26 @@ var done := false
 
 func _ready() -> void:
 	done = bool(GameState.get_flag("car_seen", false))
+	if not body_exited.is_connected(_on_body_exited):
+		body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("player") or done:
 		return
+	InteractionManager.register_interactable(self, "Inspect car")
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		InteractionManager.unregister_interactable(self)
+
+func is_interaction_available() -> bool:
+	return not done
+
+func interact(body: Node3D) -> void:
+	if body != player or done:
+		return
 	done = true
+	InteractionManager.unregister_interactable(self)
 	_start_dialogue(dialogue, "start")
 
 func _start_dialogue(dialogue_resource: DialogueResource, start_node: String) -> void:
@@ -25,5 +40,5 @@ func _on_dialogue_ended() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	player.can_move = true
 	GameState.set_flag("car_seen", true)
-	MissionManager.complete_mission("reach_car")
+	MissionManager.complete_mission("inspect_car")
 	CheckpointManager.save_progress()
