032f9fb94e770f06cc4dae462db11a7216c235d5
Gamevalentine <anhtsizoo00@gmail.com>
2026-09-05 15:46:08 +0700

    GĐ4: make radio require E interaction

--- trigger.gd
+++ trigger.gd
@@ -7,10 +7,25 @@ var repaired := false
 
 func _ready() -> void:
 	repaired = bool(GameState.get_flag("radio_repaired", false))
+	if not body_exited.is_connected(_on_body_exited):
+		body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("player") or repaired:
 		return
+	InteractionManager.register_interactable(self, "Repair radio")
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		InteractionManager.unregister_interactable(self)
+
+func is_interaction_available() -> bool:
+	return not repaired
+
+func interact(body: Node3D) -> void:
+	if body != player or repaired:
+		return
+	InteractionManager.unregister_interactable(self)
 	_start_dialogue(dialogue, "start")
 
 func _start_dialogue(dialogue_resource: DialogueResource, start_node: String) -> void:
